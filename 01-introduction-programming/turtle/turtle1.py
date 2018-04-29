0a504cf8a6a9d6afd632e20c19445de7a371a1b3
André Ferreira <contato@andreferreira.me>
2018-04-29 00:41:16 -0300

    turtle functions

--- 01-introduction-programming/turtle/turtle1.py
+++ 01-introduction-programming/turtle/turtle1.py
@@ -1,13 +1,29 @@
-from turtle import Turtle, exitonclick
+from turtle import *
 
-turtle = Turtle()
+def triangle():
+  for i in range(3):
+    fd(100)
+    rt(120)
 
-turtle.fd(100)
-turtle.rt(90)
-turtle.fd(100)
-turtle.rt(90)
-turtle.fd(100)
-turtle.rt(90)
-turtle.fd(100)
+def square():
+  for i in range(4):
+    fd(100)
+    rt(90)
+
+def squareThing():
+  color('green')
+  for i in range(20):
+      square()
+      rt(10)
+
+def octagon():
+  for i in range(8):
+    fd(100)
+    rt(45)
+
+def circle():
+  for i in range(360):
+    fd(1)
+    rt(1)
 
 exitonclick()
