114cefe7f049aac17761868824bdcce246d13389
André Ferreira <contato@andreferreira.me>
2018-04-29 00:23:25 -0300

    turtle1

---
+++ 01-introduction-programming/turtle/turtle1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, exitonclick
+
+turtle = Turtle()
+
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+
+exitonclick()
