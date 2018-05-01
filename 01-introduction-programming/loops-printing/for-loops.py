0afb2a8ce0b552c64297cfe21ebb0b083b8cbf63
André Ferreira <contato@andreferreira.me>
2018-05-01 12:04:53 -0300

    loops-printing

---
+++ 01-introduction-programming/loops-printing/for-loops.py
@@ -0,0 +1,19 @@
+print('Hello World')
+
+def print1():
+  for i in range(10):
+    print(i)
+
+def print2():
+  for i in range(1, 11):
+    print(i, end = '')
+
+def print3():
+  for i in range(4):
+    print(2*i + 1)
+
+def print4(maxnum):
+  for i in range(maxnum+1):
+    print(i)
+
+print4(11)
---
+++ 01-introduction-programming/loops-printing/while-loops.py
@@ -0,0 +1,7 @@
+def print_up_to(maxnum):
+  num = 0
+  while num <= maxnum:
+    print(num, end = ' ')
+    num += 1
+
+print_up_to(10)
